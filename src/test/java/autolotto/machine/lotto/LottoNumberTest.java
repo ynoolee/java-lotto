3e0969c9fe152a085cdcbd516c2ea694b8cd60e7
ynoolee <heerarea129@gmail.com>
2023-07-05 13:57:11 +0900

    test: LottoNumber 에 대한 엣지케이스를 테스트하는 것으로 변경
    - 1 ~ 45 라는 범위를 가진 클래스이기 때문에, 엣지인 0, 46 에 대한 테스트를 진행하는 것으로 변경

--- src/test/java/autolotto/machine/lotto/LottoNumberTest.java
+++ src/test/java/autolotto/machine/lotto/LottoNumberTest.java
@@ -1,14 +1,30 @@
 package autolotto.machine.lotto;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class LottoNumberTest {
-    @Test
-    void 숫자_1_과_45_사이의_값이_아닌_값으로_생성자_호출시_실패한다() {
-        int invalidNumber = 47;
 
-        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new LottoNumber(invalidNumber));
+    @Nested
+    class EdgeCaseContext {
+
+        @Test
+        void 숫자_45_보다_큰_값으로_생성자_호출시_실패한다() {
+            int edgeNumber = 45;
+            int invalidNumber = edgeNumber + 1;
+            Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> new LottoNumber(invalidNumber));
+        }
+
+        @Test
+        void 숫자_1_보다_작은_값으로_생성자_호출시_실패한다() {
+            int edgeNumber = 1;
+            int invalidNumber = edgeNumber - 1;
+
+            Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> new LottoNumber(invalidNumber));
+
+        }
     }
 }
