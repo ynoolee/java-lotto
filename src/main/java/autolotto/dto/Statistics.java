db0650156aedcdde20f9c615cdb5e18dd0a7e081
ynoolee <heerarea129@gmail.com>
2024-03-16 21:42:02 +0900

    refactor : 불필요한 import 제거 및 test 코드 수정

--- src/main/java/autolotto/dto/Statistics.java
+++ src/main/java/autolotto/dto/Statistics.java
@@ -2,11 +2,7 @@ package autolotto.dto;
 
 import autolotto.machine.winning.Winning;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Statistics {
 
